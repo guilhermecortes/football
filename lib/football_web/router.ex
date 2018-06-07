e3148a26c195ca27013c2617f2505cc1d8310a6d
Guilherme Cortes <guilhermecortes@gmail.com>
2018-06-07 21:33:11 +0200

    Add `leagues` routes

--- lib/football_web/router.ex
+++ lib/football_web/router.ex
@@ -20,7 +20,10 @@ defmodule FootballWeb.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", FootballWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", FootballWeb do
+    pipe_through :api
+
+    get "/leagues", LeaguesController, :index
+    get "/leagues/:division/:season", LeaguesController, :show
+  end
 end
