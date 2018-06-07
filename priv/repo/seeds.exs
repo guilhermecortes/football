10ce8209687de6221374c10ac92b2fc955c9890d
Guilherme Cortes <guilhermecortes@gmail.com>
2018-06-07 22:09:15 +0200

    Add task to populate the database with the given file `data.csv`

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,42 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Football.Transaction
+alias Football.Repo
+
+defmodule Football.Seeds do
+  def build_result(row) do
+    %Football.Result{
+      division: row.division,
+      season: row.season,
+      game_date: parse_date(row.game_date),
+      home_team: row.home_team,
+      away_team: row.away_team,
+      fthg: parse_integer(row.fthg),
+      ftag: parse_integer(row.ftag),
+      ftr: row.ftr,
+      hthg: parse_integer(row.hthg),
+      htag: parse_integer(row.htag),
+      htr: row.htr
+    }
+  end
+
+  def parse_integer(value) do
+    {result, _} = Integer.parse(value)
+    result
+  end
+
+  def parse_date(value) do
+    splitted_date = String.split(value, "/")
+    converted_date = "20#{Enum.at(splitted_date, 2)}-#{Enum.at(splitted_date, 1)}-#{Enum.at(splitted_date, 0)}"
+
+    Date.from_iso8601!(converted_date)
+  end
+end
+
+File.stream!("priv/repo/data.csv")
+  |> Stream.drop(1)
+  |> CSV.decode(headers: [:division, :season, :game_date, :home_team, :away_team, :fthg, :ftag, :ftr, :hthg, :htag, :htr])
+  |> Enum.map(fn (row) -> Football.Seeds.build_result(row) end)
+  |> Enum.each(&Repo.insert!/1)
